e20d745207baf67705fd9997978a3b2b7bcbbdd9
thanhlongtld <thanhlongtld@gmail.com>
2022-05-02 22:06:22 +0700

    first push

---
+++ src/main/java/com/ptit/sqa_project_main/SqaProjectMainApplication.java
@@ -0,0 +1,13 @@
+package com.ptit.sqa_project_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqaProjectMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqaProjectMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ptit/sqa_project_main/SqaProjectMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptit.sqa_project_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqaProjectMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
