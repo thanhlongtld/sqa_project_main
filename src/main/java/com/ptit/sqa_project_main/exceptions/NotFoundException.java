bec099e3d05a5b23b528f7eaeb74779f33dab844
thanhlongtld <thanhlongtld@gmail.com>
2022-05-08 18:09:29 +0700

    update

---
+++ src/main/java/com/ptit/sqa_project_main/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ptit.sqa_project_main.exceptions;
+
+public class NotFoundException extends Throwable {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
