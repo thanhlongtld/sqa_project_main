160bde7385adbe992b4c4ec0d59f338ef0033d09
thanhlongtld <thanhlongtld@gmail.com>
2022-05-08 13:39:55 +0700

    update

--- src/main/java/com/ptit/sqa_project_main/controllers/ReportController.java
+++ src/main/java/com/ptit/sqa_project_main/controllers/ReportController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReportController {
-    @GetMapping("/export")
+    @GetMapping("/report")
     public String index() {
         return "report";
     }
