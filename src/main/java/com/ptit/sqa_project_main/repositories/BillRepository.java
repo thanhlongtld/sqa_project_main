7f230dec67507f13c023a595a42db5eb60cdfe1c
Khanh Nguyen <khanhnd.it24@gmail.com>
2022-05-09 21:34:10 +0700

    update

--- src/main/java/com/ptit/sqa_project_main/repositories/BillRepository.java
+++ src/main/java/com/ptit/sqa_project_main/repositories/BillRepository.java
@@ -1,10 +1,13 @@
 package com.ptit.sqa_project_main.repositories;
 
 import com.ptit.sqa_project_main.models.Bill;
+import com.ptit.sqa_project_main.models.Payment;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface BillRepository extends CrudRepository<Bill, Integer> {
     List<Bill> findByClientId(Integer clientId);
+
+    List<Bill> findBillsByPaymentIsNull();
 }
---
+++ src/main/java/com/ptit/sqa_project_main/services/BillService.java
@@ -0,0 +1,18 @@
+package com.ptit.sqa_project_main.services;
+
+import com.ptit.sqa_project_main.models.Bill;
+import com.ptit.sqa_project_main.repositories.BillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BillService {
+    @Autowired
+    private BillRepository billRepository;
+
+    public List<Bill> getBillsHavePaymentNull() {
+        return (List<Bill>) this.billRepository.findBillsByPaymentIsNull();
+    }
+}
