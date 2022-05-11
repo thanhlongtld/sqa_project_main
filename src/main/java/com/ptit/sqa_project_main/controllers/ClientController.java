9043a35cbcaea5dcdbc0891c24bc86a3fa8ea756
Khanh Nguyen <khanhnd.it24@gmail.com>
2022-05-11 13:37:01 +0700

    update

--- src/main/java/com/ptit/sqa_project_main/controllers/ClientController.java
+++ src/main/java/com/ptit/sqa_project_main/controllers/ClientController.java
@@ -8,8 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -18,10 +23,27 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping("/clients")
-    public String index(Model model) {
+    public String index(Model model, @RequestParam(value = "search", required = false) String search) {
         List<Client> clients = clientService.getAll();
+        List<Client> _clients = new ArrayList<>();
+        System.out.println("Search: " + search);
+        if(search != null) {
+            try {
+                String _search = URLDecoder.decode(search, StandardCharsets.UTF_8.toString());
+                for(Client client: clients) {
+                    if(client.getName().contains(_search)) {
+                        _clients.add(client);
+                    }
+                }
+            } catch (UnsupportedEncodingException e) {
+                System.out.println(e);
+            }
+        } else {
+            _clients.addAll(clients);
+        }
 
-        model.addAttribute("clients", clients);
+        model.addAttribute("clients", _clients);
+        model.addAttribute("search", search);
 
         return "client-management";
     }
--- src/main/java/com/ptit/sqa_project_main/repositories/ClientRepository.java
+++ src/main/java/com/ptit/sqa_project_main/repositories/ClientRepository.java
@@ -3,5 +3,8 @@ package com.ptit.sqa_project_main.repositories;
 import com.ptit.sqa_project_main.models.Client;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ClientRepository extends CrudRepository<Client, Integer> {
+    List<Client> getClientsByNameLike(String name);
 }
